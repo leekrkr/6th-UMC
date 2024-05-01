c8603206b67a89a07257e4910d45f4d812b3b614
LeeJooyeon <smilejuju@ewha.ac.kr>
2024-05-01 23:49:22 +0900

    feat. make input form

--- week5/movie5/src/pages/SignUppage/SignUppage.jsx
+++ week5/movie5/src/pages/SignUppage/SignUppage.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import {
+    SignupContainer,
+    SignupTitle,
+    SignupForm,
+    Fullname,
+    Email,
+    Age,
+    Password,
+    PasswordConfirm,
+    Submit
+  
+  } from './SignupStyle';
 
 export default function SignUppage() {
 
     return(
-        <div></div>
+        <SignupContainer>
+            <SignupTitle>회원가입 페이지</SignupTitle>
+            <SignupForm>
+                <Fullname  type="text" placeholder=" 이름을 입력해주세요"/>
+                <Email  type="text" placeholder=" 이메일을 입력해주세요" />
+                <Age  type="text"  placeholder=" 나이를 입력해주세요"/>
+                <Password  type="text"  placeholder=" 비밀번호를 입력해주세요"/>
+                <PasswordConfirm  type="text"  placeholder=" 비밀번호 확인" />
+                <Submit type="button">제출하기</Submit>
+            </SignupForm>
+        </SignupContainer>
     );
 }
---
+++ week5/movie5/src/pages/SignUppage/SignupStyle.jsx
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const SignupContainer = styled.div`
+
+`;
+
+export const Submit = styled.button`
+width : 500px;
+height : 55px;
+border-radius : 25px;
+text-align: center;
+margin-left : 33%;
+color : black;
+font-size : 18px;
+font-family: var(--font);
+
+`;
+
+export const SignupTitle = styled.div`
+color : white;
+font-weight : bold;
+font-size : 20px;
+text-align: center;
+justify-content: center;
+align-items: center;
+margin-top : 40px;
+`;
+
+export const SignupForm = styled.div`
+margin-top : 40px;
+justify-content: center;
+align-items: center;
+`;
+
+export const Fullname = styled.input`
+width : 500px;
+height : 40px;
+border-radius : 25px;
+margin-left : 33%;
+margin-bottom : 20px;
+font-style: oblique;
+font-size : 15px;
+font-family: var(--font);
+color: rgb(208, 208, 208);
+
+`;
+
+export const Email = styled.input`
+width : 500px;
+height : 40px;
+border-radius : 25px;
+margin-left : 33%;
+margin-bottom : 20px;
+font-style: oblique;
+  font-size : 15px;
+  font-family: var(--font);
+  color: rgb(208, 208, 208);
+
+`;
+
+export const Age = styled.input`
+width : 500px;
+height : 40px;
+border-radius : 25px;
+margin-left : 33%;
+margin-bottom : 20px;
+font-style: oblique;
+  font-size : 15px;
+  font-family: var(--font);
+  color: rgb(208, 208, 208);
+
+`;
+
+export const Password = styled.input`
+width : 500px;
+height : 40px;
+border-radius : 25px;
+font-style: oblique;
+margin-left : 33%;
+margin-bottom : 20px;
+  font-size : 15px;
+  font-family: var(--font);
+  color: rgb(208, 208, 208);
+
+`;
+
+export const PasswordConfirm = styled.input`
+width : 500px;
+height : 40px;
+border-radius : 25px;
+margin-left : 33%;
+margin-bottom : 20px;
+font-style: oblique;
+  font-size : 15px;
+  font-family: var(--font);
+  color: rgb(208, 208, 208);
+
+`;
